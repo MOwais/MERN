c8bea41cd681ff42bee9923c5cd567b52f83bc6d
Mohammed Ahmed <moahmed@asu.edu>
2020-10-02 15:37:27 -0400

    with mongodb conn

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/',(req,res)=> res.send('API RUNNING'));
-const PORT = process.env.PORT || 5000;
+connectDB();
 
+app.get('/',(req,res)=> res.send('API RUNNING'));
+const PORT = process.env.PORT || 50000;
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
