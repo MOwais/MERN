ea556041331d5c0f8991d3168444cffef5b87900
Mohammed Ahmed <moahmed@asu.edu>
2020-10-05 16:33:42 -0400

    initial routes

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 connectDB();
 
+//init middleware
+app.use(express.json({extended:false}));
+
 app.get('/',(req,res)=> res.send('API RUNNING'));
-const PORT = process.env.PORT || 50000;
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
