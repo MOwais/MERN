2756518e9cc02e259bf7bb278207c826c0a37344
Owais Ahmed <owace.ahmed@gmail.com>
2021-02-20 19:42:44 -0500

    login screen - rename button from signup to login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -41,7 +41,7 @@ export const Login = () => {
                     required 
                 />
             </div>
-            <input type="submit" className="btn btn-primary" value="Register" />
+            <input type="submit" className="btn btn-primary" value="Login" />
             </form>
             <p className="my-1">
                 Don't have an account? <Link to="register">Sign Up</Link>
