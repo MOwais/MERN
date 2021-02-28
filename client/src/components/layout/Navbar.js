4dd0fe6793d49d514a532ad03e3446f24cf45a30
Owais Ahmed <owace.ahmed@gmail.com>
2021-02-28 15:32:32 -0500

    redirect to dashboard if logged in. Dashboard navbar icon

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,10 @@ export const Navbar = props => {
 
     const authLinks = (
         <ul>
+            <li><Link to='/dashboard'>
+                <i className="fas fa-user"></i>{' '}
+                <span className='hide-sm'>Dashboard</span></Link>
+            </li>
             <li>
                 <a onClick={props.logoutAction} href='#!'>
                     <i className="fas fa-sign-out-alt"></i>{' '}
