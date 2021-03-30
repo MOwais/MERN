30af21b5089dba3706ff5afdc494c16ecb424dad
Owais Ahmed <owace.ahmed@gmail.com>
2021-03-30 18:40:23 -0400

    posts - like, unlike post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,19 +1,63 @@
 import axios from 'axios';
 import { setAlertAction } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 //Get Posts
 
 export const getPostsAction = () => async dispatch => {
     try {
-        const res = await axios.get('/api/posts');
+        const resp = await axios.get('/api/posts');
 
         dispatch({
             type:GET_POSTS,
-            payload:res.data
+            payload:resp.data
         })
     } catch (err) {
         console.error("GET POSTS ACTION ERROR ", err);
+        dispatch({
+            type:POST_ERROR,
+            payload: { msg: err.response.statusText, status:err.response.status}
+        });
+    }
+}
+
+//Add like
+
+export const addLikesAction = postId => async dispatch => {
+    try {
+        const resp = await axios.put(`/api/posts/like/${postId}`);
+
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{
+                postId,
+                likes:resp.data
+            }
+        })
+    } catch (err) {
+        console.error("UPDATE LIKE ACTION ERROR ", err);
+        dispatch({
+            type:POST_ERROR,
+            payload: { msg: err.response.statusText, status:err.response.status}
+        });
+    }
+}
+
+//Add like
+
+export const removeLikesAction = postId => async dispatch => {
+    try {
+        const resp = await axios.put(`/api/posts/unlike/${postId}`);
+
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{
+                postId,
+                likes:resp.data
+            }
+        })
+    } catch (err) {
+        console.error("REMOVE LIKE ACTION ERROR ", err);
         dispatch({
             type:POST_ERROR,
             payload: { msg: err.response.statusText, status:err.response.status}
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,7 @@
 import { 
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 }
 from '../actions/types';
 
@@ -17,15 +18,23 @@ export default (state = initialState, action ) => {
 
     switch(type){
         case GET_POSTS:
-            return{
+            return {
                 ...state,
                 posts:payload,
                 loading:false
             }
         case POST_ERROR:
-            return{
+            return {
                 ...state,
                 error:payload,
+                loading:false
+            }
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post => (
+                    post._id === payload.postId ? {...post, likes: payload.likes } : post)
+                ),
                 loading:false
             }
         default:
